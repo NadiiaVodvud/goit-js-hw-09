74e6b2ab189393a04292650d7190a25c5301b9d6
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-29 19:10:25 +0200

    task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,19 @@
-const date = Date.now();
-// const date = new Date(70000000);
-console.log(date);
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-setTimeout(() => {
-  const date = new Date();
-}, 3000);
+const calendar = document.querySelector('#datetime-picker');
+
+DateTime = {
+  enableTime: true,
+  minTime: '00:00:00',
+  maxTime: '23:59:59',
+};
+console.log(calendar);
+// const date = Date.now();
+// const date = new Date(1650000000000);
+// console.log(date);
+
+// setTimeout(() => {
+//   const date = new Date();
+// }, 3000);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,48 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    let result = { position, delay };
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
 
-// let timeoutId = null;
+form.addEventListener('submit', onSubmitClick);
 
-// NOTIFICATION_DELAY = 3000;
-// timeoutId = setTimeout(() => {
-//   console.log();
-//   // hideNotification();
-// }, NOTIFICATION_DELAY);
+function onSubmitClick(e) {
+  e.preventDefault();
 
-// clearTimeout(timeoutId);
+  const { delay, step, amount } = e.currentTarget.elements;
+
+  let firstDelayValue = Number(delay.value);
+  let delayStepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    let position = i;
+
+    createPromise(position, firstDelayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    firstDelayValue += delayStepValue;
+  }
+}
