5b60481792cf2b35bdb941bbff6e1570413f88f0
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-29 17:10:54 +0000

    Deploying to gh-pages from @ NadiiaVodvud/goit-js-hw-09@74e6b2ab189393a04292650d7190a25c5301b9d6 🚀

--- 02-timer.77c0ce1e.js
+++
@@ -1,2 +0,0 @@
-const e=Date.now();console.log(e),setTimeout((()=>{new Date}),3e3);
-//# sourceMappingURL=02-timer.77c0ce1e.js.map
--- 02-timer.7ecbd193.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=Date.now();console.log(e),setTimeout((function(){new Date}),3e3)}();
-//# sourceMappingURL=02-timer.7ecbd193.js.map
