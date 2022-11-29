1de1d88ecb28cee163285afcf89db8e8b39e6357
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-29 16:56:46 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,17 +8,20 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startBtn.addEventListener('click', () => {
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
   timerId = setInterval(() => {
     const randomColor = getRandomHexColor();
     document.body.style.backgroundColor = randomColor;
   }, 1000);
   startBtn.disabled = true;
   stopBtn.disabled = false;
-});
+}
 
-stopBtn.addEventListener('click', () => {
+function onStopBtnClick() {
   clearInterval(timerId);
   startBtn.disabled = false;
   stopBtn.disabled = true;
-});
+}
